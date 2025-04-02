0358e180cc27dc20f9df6a4e04a00fb968b79ac1
123-4sachi <ssinfo235@gmail.com>
2025-04-02 15:08:22 +0530

    done

--- ternary-way!/index.js
+++ ternary-way!/index.js
@@ -8,7 +8,7 @@ console.log(greeting);
 
 
 let age = 22;
-let message = age >= 21 ? "You're an adult" : "You're a minor;"
+let message = age >= 21 ? "You're an adult" : "You're a minor";
 
 console.log(message);
 
@@ -16,3 +16,11 @@ let fees = 22009;
 let depofees = fees >= 4500 ? 10 : 0;
 
 console.log(`Your Total Fees is $${fees - fees * (depofees/100)}`);
+
+
+
+let EXs = 13; //input value (how much are we giving)
+
+let gfs = EXs >= 10 ? "abhi exs are more than crush" : "balnce hai";
+
+console.log(gfs);
