25030af3f009163105afba13bebeebdf3c7bf1d2
123-4sachi <ssinfo235@gmail.com>
2025-04-24 14:36:33 +0530

    while loops

---
+++ while-loops/whileloops.js
@@ -0,0 +1,9 @@
+//studying while loops
+
+let username = "";
+while(username ==="" || username === null){
+    username = window.prompt(`You didn't enter your name`);
+}
+
+
+ console.log(`Hello ${username}`);
